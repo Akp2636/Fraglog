d38b974e13f0e7f37cf8aafd07080403b07da70b
AYUSH KUMAR PANDEY <ayushpan263@gmail.com>
2026-03-10 03:30:50 +0530

    ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,34 @@
 import { Routes, Route } from 'react-router-dom'
-import Navbar      from './components/Navbar'
-import Home        from './pages/Home'
-import Profile     from './pages/Profile'
-import Library     from './pages/Library'
-import GamePage    from './pages/GamePage'
-import Discover    from './pages/Discover'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Library from './pages/Library'
+import GamePage from './pages/GamePage'
+import Discover from './pages/Discover'
 import AuthCallback from './pages/AuthCallback'
-import NotFound    from './pages/NotFound'
+import NotFound from './pages/NotFound'
 
 export default function App() {
-  return (
-    <div style={{ minHeight: '100vh', background: '#0f0f17' }}>
-      <Navbar />
-      <Routes>
-        <Route path="/"                element={<Home />} />
-        <Route path="/auth/callback"   element={<AuthCallback />} />
-        <Route path="/profile/:steamId" element={<Profile />} />
-        <Route path="/library/:steamId" element={<Library />} />
-        <Route path="/game/:appId"     element={<GamePage />} />
-        <Route path="/discover"        element={<Discover />} />
-        <Route path="*"                element={<NotFound />} />
-      </Routes>
-    </div>
-  )
+return (
+<div style={{ minHeight: '100vh', background: '#0f0f17' }}>
+
+```
+  <Navbar />
+
+  {/* Offset for fixed navbar (64px height) */}
+  <div style={{ paddingTop: 64 }}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth/callback" element={<AuthCallback />} />
+      <Route path="/profile/:steamId" element={<Profile />} />
+      <Route path="/library/:steamId" element={<Library />} />
+      <Route path="/game/:appId" element={<GamePage />} />
+      <Route path="/discover" element={<Discover />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </div>
+
+</div>
+
+)
 }
